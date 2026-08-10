30c9314400fe7f3b5a2e599cbd9ccf6758631210
millyking23 <79kpt5tsdg@privaterelay.appleid.com>
2026-08-10 23:03:45 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-import Header from "@/components/Header";
+mport Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import About from "@/components/About";
 import WhyChoose from "@/components/WhyChoose";
