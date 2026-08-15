c3026b188c83bab0176da8430d889640e4cd39e4
millyking23 <79kpt5tsdg@privaterelay.appleid.com>
2026-08-15 14:54:52 +0200

    Remove Premium+ visual layer import and restore base Monos styling

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import { Space_Grotesk, Inter, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
-import "./premium.css";
 
 const spaceGrotesk = Space_Grotesk({ subsets: ["latin"], weight: ["400", "500", "600", "700"], variable: "--font-space", display: "swap" });
 const inter = Inter({ subsets: ["latin"], weight: ["400", "500", "600", "700"], variable: "--font-inter", display: "swap" });
