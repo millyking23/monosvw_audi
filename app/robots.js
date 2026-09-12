8d6992186148134bd8d01e9705e7a6127c7d9184
millyking23 <79kpt5tsdg@privaterelay.appleid.com>
2026-09-12 13:56:55 +0200

    SEO: lock robots sitemap to www

--- app/robots.js
+++ app/robots.js
@@ -1,4 +1,4 @@
-const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || "https://www.monovwaudi.co.zw";
+const SITE_URL = "https://www.monovwaudi.co.zw";
 
 export default function robots() {
   return {
