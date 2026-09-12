2067bba0e29aab94a9fce4884ecc184b849c411a
millyking23 <79kpt5tsdg@privaterelay.appleid.com>
2026-09-12 13:57:19 +0200

    SEO: lock Bulawayo canonical to www

--- app/areas/bulawayo/page.js
+++ app/areas/bulawayo/page.js
@@ -1,4 +1,4 @@
-const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || "https://www.monovwaudi.co.zw";
+const SITE_URL = "https://www.monovwaudi.co.zw";
 
 export const metadata = {
   title: "Car Repairs & VW Audi Specialists in Bulawayo",
